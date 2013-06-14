eba456e7f01a75794886b3f385f520d4e9d37d2d
Hironobu Ueno <hironobu.ueno@gmail.com>
2013-06-14 18:32:15 +0900

    コードフォーマット

--- scraping/save.php
+++ scraping/save.php
@@ -1,14 +1,20 @@
 <?php
-$domain="http://www.yahoo.co.jp/";
-$path="index.html";
-$saveDir="out/";
+$domain = "http://www.yahoo.co.jp/";
+$path = "index.html";
+$saveDir = "out/";
 
 saveHtml($domain, $path, $saveDir);
 
-function saveHtml($domain, $path, $saveDir) {
-  $html=file_get_contents($domain . $path);
-  $fp = fopen($saveDir.$path, "w");
-  fwrite($fp, $html);
-  fclose($fp);
+/**
+ * @param $domain スクレイピング対象ドメイン
+ * @param $path スクレイピング対象URLパス
+ * @param $saveDir 保存先ディレクトリ
+ */
+function saveHtml($domain, $path, $saveDir)
+{
+    $html = file_get_contents($domain . $path);
+    $fp = fopen($saveDir . $path, "w");
+    fwrite($fp, $html);
+    fclose($fp);
 }
 
